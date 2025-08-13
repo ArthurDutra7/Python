aa83b9932346e89ea5de40ee170267e8db758890
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-08-12 22:20:02 -0300

    novo

--- exerc/estudos.py
+++ exerc/estudos.py
@@ -1,4 +1,21 @@
-print(16+10)
-print(True)
-print(False)
-print('ta')
+lista = []
+
+while True:
+    local = input("Digite o nome do local já visitado (Digite 'fim' para encerrar): ")
+    if local.lower() == "fim":
+        break
+    lista.append(local)
+
+print(f"Aqui está a lista de Locais Visitados {lista}")
+print(f"Voçê foi a {len(lista)} Locais.")
+
+procurar = input("Digite o nome do local para saber se já foi visitado ou não: ")
+if procurar in lista:
+    print(f"O local '{procurar}' foi visitado.")
+else:
+    print(f"O local '{procurar}' não foi visitado.")
+
+if lista:
+    print(f"O último dado digitado foi: {lista[-1]}")
+else:
+    print("Nenhuma palavra foi digitada.")
